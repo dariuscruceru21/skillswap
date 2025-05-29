8dd53e5587fe54e45c10110880b5c445f385b91e
Darius Cruceru <dariuscruceru@192.168.1.41>
2025-05-30 00:16:41 +0300

    fixed rontend build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),react()],
+  plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:3000', // <-- your backend port
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false
+      }
+    },
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+        },
+      },
     },
   },
 })
